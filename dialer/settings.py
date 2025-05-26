5d412655d8b9ed65c314fdf28aa12946b8de3d0a
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-05-26 18:54:43 +0530

    sdsd

--- dialer/settings.py
+++ dialer/settings.py
@@ -163,8 +163,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'users.User'
 
