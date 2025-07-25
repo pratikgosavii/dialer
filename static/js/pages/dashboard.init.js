faab4378352cb993e022f8e2ee0d48fd375c54e4
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-07-25 19:56:27 +0530

    sdsd

--- static/js/pages/dashboard.init.js
+++ static/js/pages/dashboard.init.js
@@ -83,54 +83,7 @@ chart = new ApexCharts(document.querySelector("#mini-chart4"), options);
 chart.render();
 ChartColorChange(chart, "#mini-chart4");
 
-options = {
-    series: [{
-        name: "Profit",
-        data: [12.45, 16.2, 8.9, 11.42, 12.6, 18.1, 18.2, 14.16, 11.1, 8.09, 16.34, 12.88]
-    }, {
-        name: "Loss",
-        data: [-11.45, -15.42, -7.9, -12.42, -12.6, -18.1, -18.2, -14.16, -11.1, -7.09, -15.34, -11.88]
-    }],
-    chart: {
-        type: "bar",
-        height: 400,
-        stacked: true,
-        toolbar: {
-            show: false
-        }
-    },
-    plotOptions: {
-        bar: {
-            columnWidth: "20%"
-        }
-    },
-    colors: barchartColors = getChartColorsArray("#market-overview"),
-    fill: {
-        opacity: 1
-    },
-    dataLabels: {
-        enabled: false
-    },
-    legend: {
-        show: false
-    },
-    yaxis: {
-        labels: {
-            formatter: function(r) {
-                return r.toFixed(0) + "%";
-            }
-        }
-    },
-    xaxis: {
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        labels: {
-            rotate: -90
-        }
-    }
-};
-chart = new ApexCharts(document.querySelector("#market-overview"), options);
-chart.render();
-ChartColorChange(chart, "#market-overview");
+
 
 var vectormapColors = getChartColorsArray("#sales-by-locations");
 
